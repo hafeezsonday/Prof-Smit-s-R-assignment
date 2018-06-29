e3d8eb2a639f4d4c5a4cee9d44110f9ee5b73126
Tauriq23 <3437177@myuwc.ac.za>
2018-06-29 23:40:42 +0200

    Final submission

--- assignment.R
+++ assignment.R
@@ -12,47 +12,16 @@ library(corrplot)
 # Load data ---------------------------------------------------------------
 
 Global_correlation <- read_csv("Secondary.csv")
-kenya_fertility <- read.csv("kenya_fertility.csv")
-kenya_enrollment <- read.csv("kenya_enrollment.csv")
 kenya_combo <- read.csv('Kenya_combo.csv')
 
-# plot --------------------------------------------------------------------
-
-ggplot(data = Global_correlation, aes(x = Percent_enrolled, y = TFR)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = F, colour = "hotpink")+
-  theme_pubclean()+
-  labs(title = "Figure 1: The negative correlation between female secondary education enrollment (%) and total fertitlity rate",
-       x = "Females enrolled in secondary school (%)",
-       y = "Total fertility rate (average number of children per woman)")
-
-ggplot(data = kenya_combo, aes(x = Percent_enrollment, y = TFR)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = F, colour = "hotpink")+
-  theme_pubclean()+
-  labs(title = "Figure 2: The negative correlation between female secondary education enrollment (%) and total fertitlity rate in Kenya from 1999 to 2009",
-       x = "Females enrolled in secondary school (%)",
-       y = "Total fertility rate (average number of children per woman)")
-
-#plt1 <- ggplot(data = kenya_enrollment, aes(x = Year, y = Female_School_enrollment_secondary)) +
- #geom_point() +
-  # geom_smooth(method = "lm", se = F, colour = "green") +
-  #theme_pubclean() 
-
-#plt2 <- ggplot(data = kenya_fertility, aes(x = Year, y = Total_Fertility_Rate)) +
- # geom_point() +
-  ##theme_pubclean()
-
-#library(ggpubr)
-#ggarrange(plt1, plt2, nrow = 2, ncol = 1, labels = "AUTO")
-
-
-# Normality ---------------------------------------------------------------
-
+# Assumptions ---------------------------------------------------------------
+    
 shapiro.test(Global_correlation$Percent_enrolled)
 shapiro.test(Global_correlation$TFR)
+var(Global_correlation$TFR)
+var(Global_correlation$Percent_enrolled)
 
-# Regression --------------------------------------------------------------
+# The simple linear regression equation --------------------------------------------------------------
 
 Global.lm <- lm(Percent_enrolled ~ TFR, data = Global_correlation)
 summary(Global.lm)
@@ -75,20 +44,21 @@ summary(Global.lm)
 #Multiple R-squared:  0.7245,	Adjusted R-squared:  0.7226 
 #F-statistic: 391.7 on 1 and 149 DF,  p-value: < 2.2e-16
 
+
+# A graph of the linear regression ----------------------------------------
+
 slope <- round(Global.lm$coef[2], 3)
 p.val = 0.001
 r2 <- round(summary(Global.lm)$r.squared, 3)
 
 ggplot(data = Global_correlation, aes(x = Percent_enrolled, y = TFR)) +
   geom_point() +
-  annotate("text", x = 45, y = 5, label = paste0("slope == ", slope, "~(min/min)"), parse = TRUE, hjust = 0) +
-  annotate("text", x = 45, y = 4.75, label = paste0("italic(p) < ", p.val), parse = TRUE, hjust = 0) +
-  annotate("text", x = 45, y = 4.5, label = paste0("italic(r)^2 == ", r2), parse = TRUE, hjust = 0) +
   stat_smooth(method = "lm", se = F, colour = "red") +
   theme_pubclean()+
-  labs(title = "Figure 1: The negative correlation between female secondary education enrollment (%) and total fertitlity rate",
-       x = "Females enrolled in secondary school (%)",
-       y = "Total fertility rate (average number of children per woman)")
+  theme(plot.caption = element_text(hjust = 0.5)) +
+  labs(x = "Females enrolled in secondary school (%)",
+       y = "Total fertility rate (average number of children per woman)",
+       caption = "Figure 1: The negative correlation between female secondary education enrollment (%) and total fertitlity rate")
 
 # use the accessor function to grab the coefficients:
 Global.coef <- coefficients(Global.lm)
@@ -124,22 +94,19 @@ r2 <- round(summary(Kenya.lm)$r.squared, 3)
 
 ggplot(data = kenya_combo, aes(x = Percent_enrollment, y = TFR)) +
   geom_point() +
-  annotate("text", x = 45, y = 5, label = paste0("slope == ", slope, "~(min/min)"), parse = TRUE, hjust = 0) +
-  annotate("text", x = 45, y = 4.75, 
-           label = paste0("italic(p) < ", p.val), parse = TRUE, hjust = 0) +
-  annotate("text", x = 45, y = 4.5, label = paste0("italic(r)^2 == ", r2), parse = TRUE, hjust = 0) +
   stat_smooth(method = "lm", se = F, colour = "red") +
   theme_pubclean()+
-  labs(title = "Figure 2: The negative correlation between female secondary education enrollment (%) and total fertitlity rate in Kenya from 1999 to 2009",
-       x = "Females enrolled in secondary school (%)",
-       y = "Total fertility rate (average number of children per woman)")
+  theme(plot.caption = element_text(hjust = 0.5)) +
+  labs(x = "Females enrolled in secondary school (%)",
+       y = "Total fertility rate (average number of children per woman)",
+       caption = "Figure 2: The negative correlation between female secondary education enrollment (%) and total fertitlity rate in Kenya from 1999 to 2009")
 
 # use the accessor function to grab the coefficients:
 Kenya.coef <- coefficients(Kenya.lm)
 Kenya.coef
 
 
-cor.test(Kenya.coef$Percent_enrollment, Kenya.coef$TFR, method = "kendall")
+cor.test(kenya_combo$Percent_enrollment, kenya_combo$TFR, use = "everything", method = "pearson")
 
 
 
