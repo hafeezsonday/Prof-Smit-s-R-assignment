845bcfafef9290bf5af6616938bf481b31bd83ec
Tauriq23 <3437177@myuwc.ac.za>
2018-07-02 16:08:36 +0200

    Final R Assignment Submission

--- assignment.R
+++ assignment.R
@@ -14,7 +14,7 @@ library(corrplot)
 Global_correlation <- read_csv("Secondary.csv")
 kenya_combo <- read.csv('Kenya_combo.csv')
 
-# Assumptions ---------------------------------------------------------------
+# Assumptions for Global data---------------------------------------------------------------
     
 shapiro.test(Global_correlation$Percent_enrolled)
 shapiro.test(Global_correlation$TFR)
@@ -25,25 +25,6 @@ var(Global_correlation$Percent_enrolled)
 
 Global.lm <- lm(Percent_enrolled ~ TFR, data = Global_correlation)
 summary(Global.lm)
-
-#Call:
-#lm(formula = Percent_enrolled ~ TFR, data = Global_correlation)
-
-#Residuals:
-#  Min      1Q  Median      3Q     Max 
-#-34.370  -8.623   0.558   8.521  41.307 
-
-#Coefficients:
-#  Estimate Std. Error t value Pr(>|t|)    
-#(Intercept) 113.7353     2.5891   43.93   <2e-16 ***
-#  TFR         -16.5220     0.8348  -19.79   <2e-16 ***
-#  ---
-#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-#Residual standard error: 14.26 on 149 degrees of freedom
-#Multiple R-squared:  0.7245,	Adjusted R-squared:  0.7226 
-#F-statistic: 391.7 on 1 and 149 DF,  p-value: < 2.2e-16
-
 
 # A graph of the linear regression ----------------------------------------
 
@@ -60,30 +41,19 @@ ggplot(data = Global_correlation, aes(x = Percent_enrolled, y = TFR)) +
        y = "Total fertility rate (average number of children per woman)",
        caption = "Figure 1: The negative correlation between female secondary education enrollment (%) and total fertitlity rate")
 
-# use the accessor function to grab the coefficients:
-Global.coef <- coefficients(Global.lm)
-Global.coef
 
-#(Intercept)         TFR 
-#113.73531   -16.52198 
+# Global data correlation -------------------------------------------------
 
 cor.test(Global_correlation$Percent_enrolled, Global_correlation$TFR, method = "kendall")
 
-#Kendall's rank correlation tau
-
-#data:  Global_correlation$Percent_enrolled and Global_correlation$TFR
-#z = -9.7068, p-value < 2.2e-16
-#alternative hypothesis: true tau is not equal to 0
-#sample estimates:
-#tau 
-#-0.5330448 
-
-# Normality ---------------------------------------------------------------
+# Assumptions for Kenya data ---------------------------------------------------------------
 
 shapiro.test(kenya_combo$Percent_enrollment)
 shapiro.test(kenya_combo$TFR)
+var(kenya_combo$Percent_enrollment)
+var(kenya_combo$TFR)
 
-# Regression --------------------------------------------------------------
+# Kenya Regression --------------------------------------------------------------
 
 Kenya.lm <- lm(Percent_enrollment ~ TFR, data = kenya_combo)
 summary(Kenya.lm)
@@ -101,12 +71,10 @@ ggplot(data = kenya_combo, aes(x = Percent_enrollment, y = TFR)) +
        y = "Total fertility rate (average number of children per woman)",
        caption = "Figure 2: The negative correlation between female secondary education enrollment (%) and total fertitlity rate in Kenya from 1999 to 2009")
 
-# use the accessor function to grab the coefficients:
-Kenya.coef <- coefficients(Kenya.lm)
-Kenya.coef
 
+# Kenya correlation -------------------------------------------------------
 
-cor.test(kenya_combo$Percent_enrollment, kenya_combo$TFR, use = "everything", method = "pearson")
+kenya_pearson <- cor.test(kenya_combo$Percent_enrollment, kenya_combo$TFR, use = "everything", method = "pearson")
 
 
 
